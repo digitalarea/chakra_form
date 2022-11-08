edd02d25ef097286b51289bc835c05fa11939525
digitalarea <58404779+digitalarea@users.noreply.github.com>
2022-11-08 19:58:55 +0300

    upd: issue #3 (added rect-hook-form and yup validation)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { Form } from './components/Form';
 
 function App() {
+  const theme = extendTheme({
+    components: {
+      Container: {
+        variants: {
+          'ite-container': {
+            maxW: 'sm',
+            borderRadius: 4,
+            borderWidth: 1,
+            p: 6,
+            boxShadow: 'lg',
+          },
+        },
+      },
+      Heading: {
+        variants: {
+          'ite-heading': {
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            fontSize: 'md',
+            mb: 5,
+            color: 'gray.500',
+          },
+        },
+      },
+      FormLabel: {
+        variants: {
+          'ite-label': {
+            color: 'gray.600',
+            mb: 1,
+          },
+        },
+      },
+    },
+  });
+
   return (
     <ChakraProvider theme={theme}>
       <Form />
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,51 +3,155 @@ import {
   Flex,
   Box,
   Container,
-  FormControl,
-  FormLabel,
   Input,
   Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const Form = () => {
+  const schema = yup
+    .object({
+      firstName: yup
+        .string()
+        .required('Введите имя')
+        .min(2, 'Минимум 2 символа'),
+      lastName: yup
+        .string()
+        .required('Введите фамилию')
+        .min(2, 'Минимум 2 символа'),
+      middleName: yup
+        .string()
+        .required('Введите отчество')
+        .min(2, 'Минимум 2 символа'),
+      mediaName: yup.string().required('Введите название сми'),
+      mediaLicense: yup.string().required('Введите номер лицензии'),
+      job: yup.string().required('Введите номер лицензии'),
+      email: yup.string().email('Неправильный формат email').required('Введите email'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, },
+  } = useForm({
+    mode: 'onBlur',
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = data => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <Flex minH="100vh" w="full" align="center" justify="center">
-      <Container maxW="sm">
-        <Heading
-          as="h1"
-          align="center"
-          textTransform="uppercase"
-          size="md"
-          py={5}
-        >
+      <Container variant="ite-container">
+        <Heading as="h1" variant="ite-heading">
           Аккредитация СМИ
         </Heading>
-        <Box as="form">
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Имя" />
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors?.firstName} mb={4}>
+            <FormLabel variant="ite-label">Имя</FormLabel>
+            <Input
+              type="text"
+              size="sm"
+              placeholder="Иван"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('firstName')}
+            />
+            <FormErrorMessage>{errors?.firstName?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.lastName} mb={4}>
+            <FormLabel variant="ite-label">Фамилия</FormLabel>
+            <Input
+              type="text"
+              size="sm"
+              placeholder="Иванов"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('lastName')}
+            />
+            <FormErrorMessage>{errors?.lastName?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.middleName} mb={4}>
+            <FormLabel variant="ite-label">Отчество</FormLabel>
+            <Input
+              type="text"
+              size="sm"
+              placeholder="Иванович"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('middleName')}
+            />
+            <FormErrorMessage>{errors?.middleName?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.mediaName} mb={4}>
+            <FormLabel variant="ite-label">Наименование СМИ</FormLabel>
+            <Input
+              type="text"
+              color="gray.500"
+              size="sm"
+              placeholder="Ультра (Ultra)"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('mediaName')}
+            />
+            <FormErrorMessage>{errors?.mediaName?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.mediaLicense} mb={4}>
+            <FormLabel variant="ite-label">№ свидетельства</FormLabel>
+            <Input
+              type="text"
+              color="gray.500"
+              size="sm"
+              placeholder="ЭЛ № ФС 77"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('mediaLicense')}
+            />
+            <FormErrorMessage>{errors?.mediaLicense?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.job} mb={4}>
+            <FormLabel variant="ite-label">Должность</FormLabel>
+            <Input
+              type="text"
+              color="gray.500"
+              size="sm"
+              placeholder="Редактор"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('job')}
+            />
+            <FormErrorMessage>{errors?.job?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={errors?.email} mb={4}>
+            <FormLabel variant="ite-label">Email</FormLabel>
+            <Input
+              type="email"
+              color="gray.500"
+              size="sm"
+              placeholder="example@example.ru"
+              _placeholder={{ opacity: 0.4 }}
+              {...register('email')}
+            />
+            <FormErrorMessage>{errors?.email?.message}</FormErrorMessage>
+          </FormControl>
+
+          <Box>
+            <Button type="submit" minW="full" colorScheme="blue">
+              Подать заявку
+            </Button>
           </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Фамилия" />
-          </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Отчество" />
-          </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Наименование СМИ" />
-          </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="№ свидетельства" />
-          </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Должность" />
-          </Box>
-          <Box py={1.5}>
-            <Input color="gray.500" placeholder="Email" />
-          </Box>
-          <Box py={1.5}>
-            <Button minW="full" colorScheme='blue'>Подать заявку</Button>
-          </Box>
-        </Box>
+        </form>
       </Container>
     </Flex>
   );
